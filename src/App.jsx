6bcd216da4f33849324d04d6ca7a9d43758b54e7
saiem-jatri <shahedsaiem.jatri@gmail.com>
2024-01-11 11:34:34 +0600

    reducer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import MyComponent from './components/useEffect/MyComponent.jsx'
 import Form from './components/useRef/Form.jsx'
+import Reducer from './components/useReducer/Reducer.jsx'
 export default function App() {
 const [show, setShow] = useState(true)
 
         return (
           <div className="app">
+            <Reducer/>
             <Form/>
              <div>{show && <MyComponent />}</div>
 
